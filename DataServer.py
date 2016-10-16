89ff8598e1e18d2dff7858876acef8cd2b6a8dfc
Akash Sharma S <present.akash@gmail.com>
2016-10-16 23:41:24 +0530

    Okay, so the circular dependency obviously wasn't fixed in the last commit. Tried to fix it now. It isn't working. Bored. Will try tomorrow. -_-

--- DataServer.py
+++ DataServer.py
@@ -1,6 +1,15 @@
+from flask import request
+from flask import Blueprint
 from manage import app
 
-@app.route("/book/<book_id>")
+main_blueprint = Blueprint("main", __name__)
+
+import db_util
+@main_blueprint.route("/book/<book_id>")
 def get_book(book_id):
 	book = db_util.get_book(book_id)
 	return "Getting book id: " + book.book_name
+
+@main_blueprint.route("/book", methods = ["POST"])
+def add_book():
+	return request.data
