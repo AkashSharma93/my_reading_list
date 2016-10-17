1cd61534663e2df2fe9467e4570a018f8dc905ba
Akash Sharma S <present.akash@gmail.com>
2016-10-17 15:58:17 +0530

    Fixed the circular import conflict. Finally. ^^

--- manage.py
+++ manage.py
@@ -2,12 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 
+
 app = Flask(__name__)
 manager = Manager(app)
 db = SQLAlchemy(app)
 
-from DataServer import main_blueprint
+
+from data_server import main_blueprint
 app.register_blueprint(main_blueprint)
+
 
 if __name__ == '__main__':
 	manager.run()
