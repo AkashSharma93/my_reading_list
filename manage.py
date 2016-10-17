7f12750061be0144417c28deb62607eafa9b05bb
Akash Sharma S <akash@Akashs-MacBook-Air.local>
2016-10-17 23:27:25 +0530

    Added a bit of front-end template stuff.

--- manage.py
+++ manage.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 manager = Manager(app)
 db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
 
 
 from data_server import main_blueprint
 app.register_blueprint(main_blueprint)
+
+from views import view_blueprint
+app.register_blueprint(view_blueprint)
 
 
 if __name__ == '__main__':
