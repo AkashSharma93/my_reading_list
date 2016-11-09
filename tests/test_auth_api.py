354593ddf8f636faf75632a6f155abdac5171a4a
Akash Sharma S <akash@Akashs-MacBook-Air.local>
2016-11-09 23:33:21 +0530

    Added token to the register api response. Added multiple tests to validate the confirm API.

--- tests/test_auth_api.py
+++ tests/test_auth_api.py
@@ -3,7 +3,7 @@ import unittest
 from flask import url_for, json
 
 from app import create_app
-from app.persistence import db
+from app.persistence import db, db_util
 
 
 class AuthApiTestCase(unittest.TestCase):
@@ -93,7 +93,7 @@ class AuthApiTestCase(unittest.TestCase):
         data = response.get_data(as_text=True)
         self.assertIsNotNone(data)
         data = json.loads(data)
-        for key in ("id", "email", "username", "url", "message"):
+        for key in ("id", "email", "username", "url", "message", "token"):
             self.assertIn(key, data)
 
         self.assertEqual(data["username"], request_dict["username"])
@@ -155,3 +155,125 @@ class AuthApiTestCase(unittest.TestCase):
         data = json.loads(data)
         self.assertIn("Error", data)
         self.assertEqual(data["Error"], "User with email <%s> not found." % "test_email@gmail.com")
+
+    def test_confirm_api_invalid_password(self):
+        # Check response when password is invalid.
+        request_dict = {
+            "email": "test_email@email.com",
+            "username": "test_username",
+            "password": "test_password"
+        }
+        request_data = json.dumps(request_dict)
+        response = self.client.post(url_for("api_auth_blueprint.register"), data=request_data)
+        self.assertEqual(response.status_code, 200)
+
+        data = response.get_data(as_text=True)
+        self.assertIsNotNone(data)
+        data = json.loads(data)
+
+        token = data["token"]
+        request_dict["password"] = "wrong_password"
+        request_data = json.dumps(request_dict)
+        response = self.client.post(url_for("api_auth_blueprint.confirm", token=token), data=request_data)
+        self.assertEqual(response.status_code, 401)
+
+        data = response.get_data(as_text=True)
+        self.assertIsNotNone(data)
+        data = json.loads(data)
+        self.assertIn("Error", data)
+        self.assertEqual(data["Error"], "Invalid email or password.")
+
+    def test_confirm_api_confirm_registration(self):
+        # Check response when successful confirmation is done.
+        request_dict = {
+            "email": "test_email@email.com",
+            "username": "test_username",
+            "password": "test_password"
+        }
+        request_data = json.dumps(request_dict)
+        response = self.client.post(url_for("api_auth_blueprint.register"), data=request_data)
+        self.assertEqual(response.status_code, 200)
+
+        data = response.get_data(as_text=True)
+        self.assertIsNotNone(data)
+        data = json.loads(data)
+
+        token = data["token"]
+        registered_user = db_util.get_user(data["id"])
+        self.assertFalse(registered_user.confirmed)
+
+        request_data = json.dumps(request_dict)
+        response = self.client.post(url_for("api_auth_blueprint.confirm", token=token), data=request_data)
+        self.assertEqual(response.status_code, 200)
+
+        data = response.get_data(as_text=True)
+        self.assertIsNotNone(data)
+        data = json.loads(data)
+        self.assertIn("message", data)
+        self.assertEqual(data["message"], "You have successfully verified your account.")
+
+        self.assertTrue(registered_user.confirmed)
+
+    def test_confirm_api_reconfirm(self):
+        # Check response when successful confirmation is done, and confirmation is tried again.
+        request_dict = {
+            "email": "test_email@email.com",
+            "username": "test_username",
+            "password": "test_password"
+        }
+        request_data = json.dumps(request_dict)
+        response = self.client.post(url_for("api_auth_blueprint.register"), data=request_data)
+        self.assertEqual(response.status_code, 200)
+
+        data = response.get_data(as_text=True)
+        self.assertIsNotNone(data)
+        data = json.loads(data)
+
+        token = data["token"]
+        request_data = json.dumps(request_dict)
+        response = self.client.post(url_for("api_auth_blueprint.confirm", token=token), data=request_data)
+        self.assertEqual(response.status_code, 200)
+
+        data = response.get_data(as_text=True)
+        self.assertIsNotNone(data)
+        data = json.loads(data)
+        self.assertIn("message", data)
+        self.assertEqual(data["message"], "You have successfully verified your account.")
+
+        # Sending confirmation request again.
+        response = self.client.post(url_for("api_auth_blueprint.confirm", token=token), data=request_data)
+        self.assertEqual(response.status_code, 401)
+
+        data = response.get_data(as_text=True)
+        self.assertIsNotNone(data)
+        data = json.loads(data)
+        self.assertIn("message", data)
+        self.assertEqual(data["message"], "You have already verified your account.")
+
+    def test_confirm_api_invalid_token(self):
+        # Check response when token is invalid
+        request_dict = {
+            "email": "test_email@email.com",
+            "username": "test_username",
+            "password": "test_password"
+        }
+        request_data = json.dumps(request_dict)
+        response = self.client.post(url_for("api_auth_blueprint.register"), data=request_data)
+        self.assertEqual(response.status_code, 200)
+
+        data = response.get_data(as_text=True)
+        self.assertIsNotNone(data)
+        data = json.loads(data)
+
+        token = data["token"]
+        registered_user = db_util.get_user(data["id"])
+        request_data = json.dumps(request_dict)
+        response = self.client.post(url_for("api_auth_blueprint.confirm", token="1234"), data=request_data)
+        self.assertEqual(response.status_code, 401)
+
+        data = response.get_data(as_text=True)
+        self.assertIsNotNone(data)
+        data = json.loads(data)
+        self.assertIn("Error", data)
+        self.assertEqual(data["Error"], "The token specified is invalid.")
+        self.assertFalse(registered_user.confirmed)
